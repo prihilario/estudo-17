75415080e8917141f0e877db01911122fb4451f3
Priscila <prihilario@gmail.com>
2024-02-29 15:43:26 -0300

    initial commit

---
+++ src/main/java/br/com/priscila/estudo17/Estudo17Application.java
@@ -0,0 +1,13 @@
+package br.com.priscila.estudo17;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Estudo17Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Estudo17Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/priscila/estudo17/Estudo17ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.priscila.estudo17;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Estudo17ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
